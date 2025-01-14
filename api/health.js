adc63192e5fa3284be712d6382eb07bbc574fb32
v587yc <80672026+v587yc@users.noreply.github.com>
2025-01-15 01:00:10 +0800

    Add files via upload

---
+++ api/health.js
@@ -0,0 +1,8 @@
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    res.status(200).json({ status: 'ok' });
+}; 
